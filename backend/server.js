a6a82d83704ea50adc991fc0abfdedc332aef4e8
Rishabh <Rishabh7424960527@gmail.com>
2022-07-09 00:22:52 +0530

    fixed

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ const app = express();
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
-const path = require("path");
 
 const { notFound, errorHandler } = require("./Middleware/ErrorHandler");
 
@@ -15,21 +14,6 @@ app.use(express.json());
 connectDB();
 
 app.use("/api/user", userRoutes);
-
-const __dirname1 = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  console.log(process.env.NODE_ENV);
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is runningjnj..");
-  });
-}
 
 app.use(notFound);
 app.use(errorHandler);
